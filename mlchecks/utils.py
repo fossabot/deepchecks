977b48392deb7199a9154aaa68ac8a427f76ad25
Jonthan Liberman <jonthanliberman@Jonthans-MacBook-Pro.local>
2021-10-12 11:31:11 +0300

    v0

--- mlchecks/utils.py
+++ mlchecks/utils.py
@@ -1,4 +1,7 @@
+import base64
+import io
 from typing import Any
+from matplotlib import pyplot as plt
 import sklearn
 import catboost
 
@@ -20,3 +23,10 @@ def model_type_validation(model: Any):
     """
     if not any([isinstance(model, base) for base in SUPPORTED_BASE_MODELS]):
         raise MLChecksValueError(f'Model must inherit from one of supported models: {SUPPORTED_BASE_MODELS}')
+
+
+def get_plt_base64():
+    plt_buffer = io.BytesIO()
+    plt.savefig(plt_buffer, format='jpg')
+    plt_buffer.seek(0)
+    return base64.b64encode(plt_buffer.read()).decode("utf-8")
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,12 @@ import pytest
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.datasets import load_iris
 
+from mlchecks.base import Dataset
+
 
 @pytest.fixture(scope='session')
 def iris():
-    return load_iris()
+    return load_iris(as_frame=True)
 
 
 @pytest.fixture(scope='session')
@@ -15,3 +17,15 @@ def iris_adaboost(iris):
     Y = iris.target
     clf.fit(X, Y)
     return clf
+
+@pytest.fixture(scope='session')
+def iris_dataset(iris):
+    clf = AdaBoostClassifier()
+    frame = iris.frame
+    X = iris.data
+    Y = iris.target
+    ds = Dataset(frame, 
+                features=iris.feature_names,
+                label='target')
+    clf.fit(X, Y)
+    return ds, clf
